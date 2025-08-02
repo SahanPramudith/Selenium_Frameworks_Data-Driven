e67e3be7fce0daab6220001044fef74f4583c538
sahanpramudith <sahanpramudith466@gmail.com>
2025-08-02 13:09:02 +0530

    Add TestNG configuration for login test suite

---
+++ src/main/java/DataDrivenTest/LoginUsingParameters.java
@@ -0,0 +1,44 @@
+package DataDrivenTest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LoginUsingParameters {
+    WebDriver driver;
+    @Test
+    @Parameters({"username","password","validation"})
+    public void loginTestScenario(String password ,String username,String expectValidation) throws InterruptedException {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(" https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        WebElement useName = driver.findElement(By.xpath("//input[@placeholder='Username']"));
+        useName.sendKeys(username);
+        WebElement pasword = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        pasword.sendKeys(password);
+
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+        boolean urlVerification = driver.getCurrentUrl().contains("dashboard");
+        Thread.sleep(3000);
+
+        if (expectValidation.equals("valid")){
+            Assert.assertTrue(urlVerification,"Login success but not navigate the dashboard");
+        } else if (expectValidation.equals("Invalid")) {
+            Assert.assertFalse(urlVerification,"Expecting login is success but navigate the dashboard");
+        }
+    }
+    @AfterMethod
+    public void colseBorw(){
+        driver.quit();
+    }
+}
