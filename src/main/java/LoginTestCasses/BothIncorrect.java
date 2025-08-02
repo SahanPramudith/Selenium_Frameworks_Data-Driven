d12b0c3663678fbf320655cb0c50878030b24969
sahanpramudith <sahanpramudith466@gmail.com>
2025-08-02 07:42:43 +0530

    Add Selenium WebDriver integration and update pom.xml dependencies

--- src/main/java/LoginTestCasses/BothIncorrect.java
+++ src/main/java/LoginTestCasses/BothIncorrect.java
@@ -1,4 +1,22 @@
 package LoginTestCasses;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BothIncorrect {
+
+    WebDriver driver;
+    @Test
+    public void loginBothInCorrect(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(" https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+        WebElement useName = driver.findElement(By.name("username"));
+        useName.sendKeys("Admin");
+        WebElement password = driver.findElement(By.xpath("//input[@placeholder='Password']"));
+        password.sendKeys("admin123");
+    }
 }
--- src/main/java/LoginTestCasses/Bothcorrect.java
+++ src/main/java/LoginTestCasses/Bothcorrect.java
@@ -1,4 +1,14 @@
 package LoginTestCasses;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Bothcorrect {
+
+
+    WebDriver driver;
+    public void loginWithBothCorrect(){
+        driver=new ChromeDriver();
+    }
+
 }
